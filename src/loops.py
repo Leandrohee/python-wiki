6c64c022a82b520803797b4c22e5f8937a886b4b
leandroTorres <leandrohenrique_@live.com>
2025-02-09 22:55:40 -0300

    feat: loops

--- src/loops.py
+++ src/loops.py
@@ -1,30 +1,39 @@
 #Simple loop with number 1 to 5
 for i in range(1,6):
-    print(i)                                # 1-2-3-4-5
+   i                              # 1-2-3-4-5
 
 #Simple loop with numbers 0 to 4
 for i in range(5):
-    print(i)                                # 0-1-2-3-4
+    i                            # 0-1-2-3-4
 
 #Loop with an list
 list1 = ["arroz", "banana", "feijao"]
 for food in list1:
-    print(i)                                # "arroz"-"banana"-"feijao"
+   i                              # "arroz"-"banana"-"feijao"
 
 #Loop over a string
 word = "leandro"
 for letter in word:
-    print(letter)                           # l-e-a-n-d-r-o
+   letter                          # l-e-a-n-d-r-o
 
 #Loop with while
 num = 1
 while num <= 5:
-    print(num)
+    num
     num += 1
 
 #Loop with a break
 for num in range(10):
-    print(num)                              # 0-1-2-3-4
+    # print(num)
+    num                              # 0-1-2-3-4
     if num == 5:
         break
+
+#Getting the index of an list 
+list_example = [10,20,30,40,50]
+for index, item in enumerate(list_example):
+    # print(f"the item {index} is {item}")
+    item
+
+
 
